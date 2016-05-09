bea7050226b3b83249171044276558e14caf2358
JocelynMichel <jocelyn.michel77@gmail.com>
2016-05-09 20:49:45 +0200

    Revert "Revert "Ajout test bluetooth""
    This reverts commit fd2521d2ac733a194b5fd6472e406fc770bbf165.

--- app/src/main/java/csid/sharedeck/MenuActivity.java
+++ app/src/main/java/csid/sharedeck/MenuActivity.java
@@ -2,6 +2,10 @@ package csid.sharedeck;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.*;
+import android.content.*;
+import android.view.*;
+import android.view.View.*;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        final Button loginButton = (Button) findViewById(R.id.button2);
+        loginButton.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuActivity.this, TestActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
